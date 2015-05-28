c08b48f09e547053874f6d6600002e088e25b0dc
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-05-28 15:53:20 +0800

    bug fix. should consider equal.

--- quicksort.hs
+++ quicksort.hs
@@ -1,8 +1,8 @@
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) = left ++ [x] ++ right
-  where left  = quicksort [n | n <- xs, n < x]
+  where left  = quicksort [n | n <- xs, n <= x]
         right = quicksort [n | n <- xs, n > x]
 
-test_quicksort = quicksort [1,2,3,8,6,4]
+test_quicksort = quicksort [1,2,3,8,6,4,2]
 test_quicksort'= quicksort "aasdfijwe"
