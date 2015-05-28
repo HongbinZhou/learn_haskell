0c92562facba678e990d8fc0f9876ad380b4835f
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-05-28 15:12:07 +0800

    Two players can play interactivly.
    - Can choose first player randomly
    - Player moves one by one
    - Stop when someone win
    - Continew when has invalid input

--- tictactoe/src/Main.hs
+++ tictactoe/src/Main.hs
@@ -1,10 +1,51 @@
 import Data.Foldable
 import Data.Maybe
+import System.Random
 
--- import Control.Monad.State
 
 data Player = X | O
-            deriving (Eq, Show)
+            deriving (Eq, Bounded, Enum, Show)
+
+-- Learn random
+-- ref: http://en.wikibooks.org/wiki/Haskell/Libraries/Random
+instance Random Player where
+  random g = case randomR (fromEnum (minBound :: Player), 
+                           fromEnum (maxBound :: Player))
+                  g 
+             of (r, g') -> (toEnum r, g')
+
+  randoms g = case random g of
+               (a, g') -> a:(randoms g')
+
+  randomR (a,b) g = case randomR (fromEnum a, fromEnum b) g of
+                     (r, g') -> (toEnum r, g')
+
+genPlayers :: StdGen -> [Player]
+genPlayers g = 
+  let (fst, _) = random g :: (Player, StdGen) 
+  in case fst of
+      X -> concat $ repeat [X, O]
+      O -> concat $ repeat [O, X]
+
+test_genPlays = do
+  gen <- newStdGen
+  putStrLn $ show $ take 10 $ genPlayers gen
+
+test_randoms = do
+  gen <- newStdGen
+  let x = take 10 $ randoms gen :: [Player]
+  return $ show x
+
+test_random = do
+  gen <- newStdGen
+  let (x, y) = random gen :: (Player, StdGen)
+  return $ show x
+
+test_randomR = do
+  gen <- newStdGen
+  let (x, y) = randomR (X, O) gen
+  return $ show x
+
 
 data Position = Position {
   getPosition :: (Int,Int)}
@@ -27,44 +68,48 @@ data Board = Board {
 
 emptyBoard :: Board
 emptyBoard = Board posPlayer (Empty $ Step [])
-  where posPlayer = map (\x -> PosPlayer x Nothing) matrix  
+  where posPlayer = map (\x -> PosPlayer x Nothing) matrix
         matrix = [Position (x,y)| x <- [1..3], y <- [1..3]]
 
 data Step = Step [PosPlayer]
             deriving (Show)
 
 testBoard :: Board
-testBoard = 
+testBoard =
   Board [PosPlayer (Position (1,1)) (Just X),
-         PosPlayer (Position (1,2)) (Just X),
-         PosPlayer (Position (1,3)) (Just X),
+         PosPlayer (Position (1,2)) (Just O),
+         PosPlayer (Position (1,3)) (Just O),
          PosPlayer (Position (2,1)) Nothing,
          PosPlayer (Position (2,2)) (Just X),
          PosPlayer (Position (2,3)) Nothing,
          PosPlayer (Position (3,1)) Nothing,
          PosPlayer (Position (3,2)) Nothing,
-         PosPlayer (Position (3,3)) (Just X)]
-         (Finished $ Step [])
+         PosPlayer (Position (3,3)) Nothing]
+  -- (InPlay $ Step [])
+  (InPlay $ Step [PosPlayer (Position (1, 3)) (Just O),
+                 PosPlayer (Position (2, 2)) (Just X),
+                 PosPlayer (Position (1, 2)) (Just O),
+                 PosPlayer (Position (1, 1)) (Just X)])
 
 toPosition:: (Int, Int) -> Position
 toPosition (a, b) = Position (a, b)
 
 point2Line :: [(Int, Int)] -> Line
-point2Line x = 
+point2Line x =
   let [px,py,pz] = Position <$> x
   in Line px py pz
 
 row :: Int -> Line
-row n = point2Line [(n, x) | x <- [1..3]] 
+row n = point2Line [(n, x) | x <- [1..3]]
 
 col :: Int -> Line
-col n = point2Line [(x, n) | x <- [1..3]] 
+col n = point2Line [(x, n) | x <- [1..3]]
 
 leftX :: Line
-leftX = point2Line [(x, x) | x <- [1..3]] 
+leftX = point2Line [(x, x) | x <- [1..3]]
 
 rightX :: Line
-rightX = point2Line [(x, y) | x <- [1..3], y <- [1..3], x+y==4] 
+rightX = point2Line [(x, y) | x <- [1..3], y <- [1..3], x+y==4]
 
 allLines :: [Line]
 allLines = [f x | f <- [row, col], x <- [1,2,3]] ++
@@ -78,9 +123,16 @@ whoMakeLine b (Line px py pz) = do
      x <- playerAt b px
      y <- playerAt b py
      z <- playerAt b pz
-     if x==y && y==z 
-       then return x 
+     if x==y && y==z
+       then return x
        else Nothing
+
+
+-- whoWon :: Board -> Either (Maybe Player) String
+whoWon' :: Board -> Maybe Player
+whoWon' b =  case catMaybes $ map (whoMakeLine b) allLines of
+             [] -> Nothing
+             x:_ -> Just x
 
 whoWon :: Board -> Either (Maybe Player) String
 whoWon b = case getStatus b of
@@ -90,10 +142,10 @@ whoWon b = case getStatus b of
                   player = case catMaybes $ map (whoMakeLine b) allLines of
                              [] -> Nothing
                              x:_ -> Just x
-            
+
 playerAt :: Board -> Position -> Maybe Player
 playerAt (Board _ (Empty _)) _ = Nothing
-playerAt (Board b _) p = 
+playerAt (Board b _) p =
   case find (\(PosPlayer pos _) -> pos==p) b of
    Just (PosPlayer pos' player) -> player
    _ -> Nothing
@@ -104,24 +156,32 @@ isOccupied b p =
    Just _ -> True
    _ -> False
 
+getSteps :: Board -> [PosPlayer]
+getSteps b = 
+  case getStatus b of
+   Empty (Step x) -> x
+   InPlay (Step x) -> x
+   Finished (Step x) -> x
+
 move :: Board -> Position -> Player -> Either Board String
 move b p who =
   case getStatus b of
-   Finished _ -> Right $ "Error: can't move more for finished board!"
-   Empty (Step pp)-> Left $ Board posPlayer (status pp)
-   InPlay (Step pp) -> 
-     if isOccupied b p
-     then Right $ "Position" ++ show p ++ "was occupied. replay please! "
-     else Left $ Board posPlayer (status pp)
+    Finished _ -> Right $ "Error: can't move more for finished board!"
+    Empty    _ -> Left $ Board posPlayer status
+    InPlay   _ ->
+      if isOccupied b p
+      then Right $ "Warning: Position" ++ show p ++ " was occupied, play again!"
+      else Left $ Board posPlayer status
   where posPlayer = setPosPlayer (getPosPlayers b) p (Just who)
-        status pp = InPlay $ Step $ (PosPlayer p (Just who)):pp
-
-test_move = move emptyBoard (Position (1,2)) X
-test_takeBack = case test_move  of
-        Left b -> takeBack b
+        newStep = Step $ (PosPlayer p (Just who)): (getSteps b)
+        status = 
+          -- First do move on given board, then calc status
+          case whoWon' (Board posPlayer (InPlay newStep)) of
+           Just _ -> Finished newStep
+           _ -> InPlay newStep
 
 takeBack :: Board -> Either Board String
-takeBack b = 
+takeBack b =
   case getStatus b of
    Empty _ -> Right "Error: Can't do takeBack for Empty board!"
    InPlay (Step x) -> genNewBoard x
@@ -137,17 +197,75 @@ setPosPlayer p pos mp = foldl (\acc pp ->
                             else acc ++ [pp]) [] p
 
 instance Show Position where
-  show (Position (x, y)) = 
+  show (Position (x, y)) =
     "(" ++ show x ++ ", " ++ show y ++ ")"
 
 instance Show PosPlayer where
-  show (PosPlayer pos player) = 
+  show (PosPlayer pos player) =
     "(" ++ show pos ++ ", " ++ show player ++ ")"
 
 instance Show Board where
-  show (Board xs s) = 
+  show (Board xs s) =
     (unlines $ map showRow (group3By3 xs)) ++ show s
     where showRow = foldl (\acc x -> acc ++ show x ++ " " ) ""
           group3By3 :: [a] -> [[a]]
           group3By3 [] = []
           group3By3 x = [take 3 x] ++ group3By3 (drop 3 x)
+
+isInBoard :: Position -> Bool
+isInBoard (Position (x, y)) =
+  (x, y) `elem` [(m, n) | m <- [1..3], n <- [1..3]]
+
+posParser :: String -> Maybe (Int, Int)
+posParser cs0 =
+  case [ (x, y, cs2) | (x, cs1) <- reads cs0, (y, cs2) <- reads cs1 ] of
+   [(x, y, "")] -> if isInBoard (Position (x, y))
+                   then Just (x, y)
+                   else Nothing
+   _ -> Nothing
+
+info :: String -> String
+info = (++) ">>> INFO: "
+
+nextPlayer :: Player -> Player
+nextPlayer X = O
+nextPlayer O = X
+
+isFinished :: Board -> Bool
+isFinished b = 
+  case getStatus b of
+   Finished _  -> True
+   Empty _ -> False
+   InPlay _ ->  False
+
+loopGame :: Board -> Player -> IO()
+loopGame b who = do
+  putStrLn $ info "Current board is:"
+  putStrLn $ show b
+  if isFinished b
+    then putStrLn $ info "Game finished. Player "++ show who ++ " win!"
+    else do
+      putStrLn $ info "Player " ++ show who ++ ", input your position (eg: 1 2): "
+      pos <- getLine
+      case posParser pos of
+       Just (x, y) -> 
+         let pos = Position (x, y)
+         in do
+           putStrLn $ "Your input position is:" ++ show pos
+           case move b pos who of
+            Left b' -> loopGame b' (nextPlayer who)
+            Right s -> do
+              putStrLn s
+              loopGame b who
+       Nothing -> do
+         putStrLn "Error: Invalid input!"
+         loopGame b who
+
+
+main :: IO()
+main = do
+  putStrLn "Let's play tic tac toe game!"
+  gen <- newStdGen
+  let (fst, _) = random gen :: (Player, StdGen)
+  loopGame emptyBoard fst
+
