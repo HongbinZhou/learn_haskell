9376c2533e924cd7403557a6927d3c5de4c9a9ea
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-08-23 11:56:15 +0800

    try more turtle command.

--- sh.hs
+++ sh.hs
@@ -6,7 +6,15 @@
 import Turtle
 -- import qualified Data.Text as T
 
+datePwd :: IO UTCTime
+datePwd = do
+  dir <- pwd
+  datefile dir
+
+str :: Text
 str = "Hello"
+
+main :: IO ()
 main = do
   echo str
   echo $ str <> "zhou"
@@ -14,5 +22,24 @@ main = do
   time <- datefile dir
   print time
 
+main' = do
+  time <- datePwd
+  print time
 
+test1 :: IO ()
+test1 = do
+  _ <- proc "ls" ["-a", "-l"] empty
+  die "hi"
 
+test2 :: IO ()
+test2 = do
+  _ <- shell "ls -al" empty
+  die "hi"
+
+test3 :: IO ()
+test3 = stdout stdin
+
+getEnv  x = do
+  export  "hi" "zhouasdf"
+  e <- env
+  print $  lookup x e
