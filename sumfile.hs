54525a59d43e389c8654d6259711145a74246a2f
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-08-29 21:44:06 +0800

    Read int array from std and calc the sum.

---
+++ sumfile.hs
@@ -0,0 +1,17 @@
+
+-- | Write a list of numbers [1..n] to file
+genNumFile :: Int -> IO ()
+genNumFile n = do
+  writeFile "numbers.txt" (numList2Str [1..n])
+
+-- | [1..3] will be "1 2 3"
+numList2Str :: [Int] -> String
+numList2Str  = foldr (\x acc -> show x ++ " " ++ acc) ""
+
+-- | build: ghc --make sumfile.hs
+-- | run: cat numbers.txt | ./sumfile
+main :: IO ()
+main = do
+      contents <- getContents
+      print (sumFile contents)
+    where sumFile = sum . map read . words
