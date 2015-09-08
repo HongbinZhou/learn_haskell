55726d126e5bfcbb4dd55c0e9b945c69c82cd49d
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-09-08 10:29:30 +0800

    get file size.

--- RecursiveContents.hs
+++ RecursiveContents.hs
@@ -3,6 +3,8 @@ module RecusiveContents (getRecursiveContents) where
 import Control.Monad (forM)
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
+import System.IO
+import System.Posix (getFileStatus, fileSize, FileOffset)
 
 getRecursiveContents :: FilePath -> IO [FilePath]
 getRecursiveContents topdir = do
@@ -15,3 +17,14 @@ getRecursiveContents topdir = do
              then getRecursiveContents p
              else return [p]
   return (concat paths)
+
+-- | need to open file first
+simpleFileSize :: FilePath -> IO Integer
+simpleFileSize path =
+    withFile path ReadMode hFileSize
+
+-- | no need to open file
+getFileSize :: FilePath -> IO FileOffset
+getFileSize path = do
+    fs <- getFileStatus path
+    return $ fileSize fs
