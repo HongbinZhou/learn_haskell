712e5177b7b6164450f857563093abaa9427286c
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-09-24 13:55:44 +0800

    add quickCheck for quicksort.

--- quicksort.hs
+++ quicksort.hs
@@ -1,3 +1,4 @@
+import Test.QuickCheck
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) = left ++ [x] ++ right
@@ -6,3 +7,9 @@ quicksort (x:xs) = left ++ [x] ++ right
 
 test_quicksort = quicksort [1,2,3,8,6,4,2]
 test_quicksort'= quicksort "aasdfijwe"
+
+prop_qs :: [Int] -> Bool
+prop_qs x = quicksort x == quicksort ( quicksort x)
+
+main :: IO ()
+main = quickCheck prop_qs
